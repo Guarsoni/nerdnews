9dc8920a65e5763ac641bbde4bf4e3b604ddc732
Charizard <guilhermecjbr@gmail.com>
2015-11-16 21:52:41 -0200

    Atualização da pagina enviarcontato

--- enviarcontato.php
+++ enviarcontato.php
@@ -1,24 +1,33 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
 <?php
-$nome = $_POST["nome"];
-$email = $_POST["email"];
-$assunto = $_POST["assunto"];
-$mensagem = $_POST["comentario"];
-
-$conteudo = "Nome: $nome <br> E-mail: $email <br> Assunto: $assunto <br> Mensagem: $mensagem";
-$seuemail = "guilhermecjbr@gmail.com";
-$headers = "MIME-Version: 1.0\r\n";
-$header .= "Content-type: text/html; charset=iso-8859-1\r\n";
-$assunto = "Teste - Contato do site";
-
-$enviar = mail($seuemail, $assunto, $conteudo, $headers);
-
-if ($enviar) {
-	echo "<script type='text/java/javascript'> alert ('Contato Enviado com Sucesso!'); window.location.href='contato.php'; </script>";
-}
- else
- {
-echo "<script type='text/ javascript'> alert ('Ocorreu algum erro ao enviar o formulario'); </script>";
-
- }
+	//Recebendo os dados do formulário.
+	$nome = $_POST["nome"];
+	$email=$_POST["email"];
+	$assunto=$_POST["assunto"];
+	$mensagem=$_POST["comentario"];
+	$conteudo = "Nome: $nome <br>E-mail: $email<br> Assunto: $assunto <br> Mensagem: $mensagem";
+	$seuemail="guilhermecjbr@gmail.com";
+	$headers="MIME-Version: 1.0\r\n";
+	$headers="Content-type: text/html; charset=iso-8859-1\r\n";
+	$headers="From: $email\r\n";
+	$assunto="Teste - Contato do site";
+	$enviar= mail($seuemail, $assunto, $conteudo, $headers);
+	if($enviar){
+		echo"<script type='text/javascript'> alert('Contato Enviado com Sucesso!'); window.location.href='index.php?pagina=13';
+		</script>";}
+	else
+		{
+		echo "<script type='text/ javascript' alert('Ocorreu algum erro ao enviar o formulario');</<script>";
+		}
 ?>
+	
+</body>
+</html>
